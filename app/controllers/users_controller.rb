5d0b71f8d08dad3c14b467d4f9771dbfdaf64470
your_name <your_email>
2014-08-05 17:34:48 +0800

    添加用户页面添加按钮功能方法

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class UsersController < ApplicationController
   def welcome
-    @user = User.paginate(page: params[:page],per_page:11)
+    @user = User.paginate(page: params[:page],per_page:10)
   end
 
   def signup
@@ -13,11 +13,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
-    if @user.save
-      cookies.permanent[:token] = @user.token
-      redirect_to :welcome
+    if current_user
+      if current_user.name == "admin"
+        if @user.save
+          redirect_to  :adduser
+        else
+          render :adduser
+        end
+      end
     else
-      render :signup
+      if @user.save
+        cookies.permanent[:token] = @user.token
+        redirect_to :welcome
+      else
+        render :signup
+      end
     end
   end
 
